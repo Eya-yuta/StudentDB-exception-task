ff2ea5eef007d9bb209b20e635a8eacba42b4e0c
Eya-BHS <benhasseneya28@gmail.com>
2025-12-19 13:44:38 +0100

    Add 2 short version tests to the exception

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -18,6 +18,16 @@ class MainTest {
         }
     }
 
+    //the short version of the test returnTrueIfTheRunTimeExceptionIsThrown
+    @Test
+    void returnTrueIfTheRunTimeExceptionIsThrownShortV() {
+        Student student=new Student("1234","Mike","Maths");
+        StudentRepo studentRepo=new StudentRepo();
+        studentRepo.save(student);
+        // If exception is thrown, the test should be true.
+        assertThrows(NoStudentRunTimeException.class,()->{ studentRepo.findStudentById("3456");});
+    }
+
     @Test
     void returnTrueIfTheRunTimeExceptionIsNotThrown() {
         Student student=new Student("1234","Mike","Maths");
@@ -30,6 +40,16 @@ class MainTest {
             // If exception is thrown, the test should fail.
             fail();
         }
+    }
+
+    //the short version of the test returnTrueIfTheRunTimeExceptionIsNotThrown
+    @Test
+    void returnTrueIfTheRunTimeExceptionIsNotThrownShortV() {
+        Student student=new Student("1234","Mike","Maths");
+        StudentRepo studentRepo=new StudentRepo();
+        studentRepo.save(student);
+        // If exception is not thrown, the test should be true.
+        assertDoesNotThrow(()->{ studentRepo.findStudentById("1234");});
     }
 
 
